a4939754bf100f675b156594b0e65203e30d6333
Davi Costa <contato.davikosta@gmail.com>
2025-01-24 15:45:06 -0300

    Versão 1.0

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,48 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+// Version: 1.0
+
+// Variables
+let inputAmigo = document.getElementById('amigo');
+let listaAmigos = document.getElementById('listaAmigos');
+let resultList = document.getElementById('resultado');
+let amigos = [];
+
+// Functions
+function adicionarAmigo() {
+
+    let amigo = inputAmigo.value;
+    
+    if (amigo === '') {
+        alert('Digite o nome do amigo!');
+        return;
+    }
+
+    amigos.push(amigo);
+
+    listaAmigos.innerHTML = amigos.join(', ');
+
+    clearInput();
+    
+};
+
+function clearInput() {
+    inputAmigo.value = '';
+}
+
+function sortearAmigo() {
+
+    if (amigos.length < 2) {
+        alert('Adicione mais amigos para sortear!');
+        return;
+    }
+    
+    let tamanhoDaLista = retornarTamanhoDaLista();
+    let amigoSorteado = Math.floor(Math.random() * tamanhoDaLista);
+    let exibirSorteado = amigos[amigoSorteado];
+
+    resultList.innerHTML = exibirSorteado;
+
+}
+function retornarTamanhoDaLista() {
+    return amigos.length;
+}
+
